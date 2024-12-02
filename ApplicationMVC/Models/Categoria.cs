ab9611708cb1ae8f3d6df388038177ed29039322
Euclides Marques <euclidesmarques270@gmail.com>
2024-12-02 11:36:10 -0300

    Created Class Roupa and Categoria

---
+++ ApplicationMVC/Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApplicationMVC.Models
+{
+    public class Categoria
+    {
+        [Column("ID_CATEGORIA")]
+        public int CategoriaId { get; set; }
+
+        [Column("NM_CATEGORIA")]
+        public string NomeCategoria { get; set; }
+
+        [Column("DS_CATEGORIA")]
+        public string DescricaoCategoria { get; set; }
+
+        public List<Roupa> Roupas { get; set; }
+    }
+}
---
+++ ApplicationMVC/Models/Roupa.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApplicationMVC.Models
+{
+    public class Roupa
+    {
+        [Column("ID_ROUPA")]
+        public int RoupaId { get; set; }
+
+        [Column("NM_ROUPA")]
+        public string NomeRoupa { get; set; }
+
+        [Column("DS_ROUPA")]
+        public string DescricaoRoupa { get; set; }
+
+        [Column("VL_ROUPA")]
+        public double PrecoRoupa { get; set; }
+
+        [Column("FL_IMAGEM_ROUPA")]
+        public string ImagemUrl { get; set; }
+        
+        [Column("FL_THUMBNAIL_ROUPA")]
+        public string ThumbnailUrl { get; set; }
+
+        [Column("FL_ESTOQUE")]
+        public bool EmEstoque { get; set; }
+
+        [Column("ID_CATEGORIA")]
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria { get; set; }
+    }
+}
